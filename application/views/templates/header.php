b0c8fbe9e9a69e723740f874984d5e9ff159a258
imboyo <imbomabrur84@gmail.com>
2021-09-03 14:12:52 +0800

    init assets

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,1 +1,33 @@
-<h1>Header</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Title -->
+  <title><?php echo $title ?></title>
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="<?php echo base_url('assets\templates\favicon.ico') ?>" type="image/x-icon">
+
+  <!--plugins-->
+	<link href="<?php echo base_url() ?>assets/templates/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+	<link href="<?php echo base_url() ?>assets/templates/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+	<link href="<?php echo base_url() ?>assets/templates/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+	<link href="<?php echo base_url() ?>assets/templates/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+  <!-- loader-->
+	<link href="<?php echo base_url() ?>assets/templates/css/pace.min.css" rel="stylesheet" />
+	<script src="<?php echo base_url() ?>assets/templates/js/pace.min.js"></script>
+	<!-- Bootstrap CSS -->
+	<link href="<?php echo base_url() ?>assets/templates/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url() ?>assets/templates/css/bootstrap-extended.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="<?php echo base_url() ?>assets/templates/css/app.css" rel="stylesheet">
+	<link href="<?php echo base_url() ?>assets/templates/css/icons.css" rel="stylesheet">
+	<!-- Theme Style CSS -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/templates/css/dark-theme.css" />
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/templates/css/semi-dark.css" />
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/templates/css/header-colors.css" />
+
+</head>
+<body>
+  
