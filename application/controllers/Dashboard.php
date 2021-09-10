7a0fd41cc337d95a07d28bdf958d1aeedcd43100
imboyo <imbomabrur84@gmail.com>
2021-09-11 01:37:35 +0800

    commit

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,13 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
   public function __construct() {
     parent::__construct();
-
-    // redirect_to_login_if_not_logged_in();
   }
 
   // Home Page
-  public function index(){
-
+  public function index(){redirect_to_login_if_not_logged_in();
     $data['title'] = 'Dashboard';
     
     $this->load->view('templates/header', $data);
